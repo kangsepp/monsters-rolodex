95c36cf4a16066b0aabd42c0d2716ec5bf653b41
Risto <risto.kangsepp@gmail.com>
2020-03-01 10:41:26 +0200

    Updates

---
+++ precache-manifest.077e1de97f5c70ac06f3d8ab89b7db04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20fdab7f899d89b5ca2a7918837e8e94",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1c9a3f60bbb9f9f5f1b4",
+    "url": "/monsters-rolodex/static/css/main.2187c432.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c9a3f60bbb9f9f5f1b4",
+    "url": "/monsters-rolodex/static/js/main.c4e48823.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
